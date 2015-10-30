c004f0fcf8d23f44d32ea45c196501d85288898d
Jim Cadden <jmcadden@bu.edu>
2015-10-30 17:47:43 -0400

    Enables std::istream to read from IOBuf chain
    StreamIOBuf inherits std::basic_streambuf<char> and takes an const IOBuf pointer. StreamIOBuf can be passed in to the constructor of a standard C++ istream and the stream will source from the underlying IOBuf chain. Warning: reads on istream are all pass-by-value, so the data of the IOBuf will be copied (bummer!)

--- baremetal/src/StreamIOBuf.h
+++ baremetal/src/StreamIOBuf.h
@@ -12,40 +12,19 @@
 namespace ebbrt {
 class StreamIOBuf : public std::streambuf {
 public:
+  StreamIOBuf(const IOBuf *p);
   virtual ~StreamIOBuf() {}
 
 protected:
-  StreamIOBuf() 
-  StreamIOBuf(const StreamIOBuf &rhs) {}
-  explicit StreamIOBuf(const IOBuf *p) : p_{ p } {}
-  // operator=
-  // swap
-  //LOCAL
-  // imbue 
-  //POSITIONING
-  // StreamIOBuf* setbuf( char_type* s, std::streamsize n )
-  // seakoff
-  // seakpos
-  // sync
-  // GET AREA
-  std::streamsize showmanyc(){ return -1; }
-  int_type underflow(){ return std::char_traits<char>::eof(); }
-  //int_type uflow() // The base class version calls underflow() increments gptr().
-  std::streamsize xsgetn( char_type* s, std::streamsize count ) { return 0; }
-  char_type* eback() { return nullptr; };
-  char_type* gprt() { return nullptr; };
-  char_type* egprt() { return nullptr; };
-  void gpump( int count ) {};
-  void setg( char_type* gbeg, char_type* gcurr, char_type* gend );
-  // PUT AREA
-  // int_type overflow(int_type c){ return Traits:eof(); }
-  // std::streamsize xsputn( const char_type* s, std::streamsize count );
-  // char_type* pbase() { return nullptr; }  const;
-  // char_type* pptr() { return nullptr; }  const;
-  // char_type* epptr() { return nullptr; }  const;
-  // void pbump( int count ) {}
-  // void setp( char_type* pbeg, char_type* pend ) {}
-  // int_type pbackfail(int_type c){ return Traits:eof(); }
+  std::streamsize showmanyc();
+  int underflow();
+  // put functions
+  int_type overflow(int_type c) { return traits_type::eof(); }
+  std::streamsize xsputn(const char_type *s, std::streamsize count) {
+    return 0;
+  };
+  int_type pbackfail(int_type c) { return traits_type::eof(); }
+
 private:
   const IOBuf *p_{ nullptr };
   size_t offset_{ 0 };
